dd9a16ebba3f89d23b87f86e151d79e6bc3301ad
ibitoye oyinlade <oyinladeibitoye@gmail.com>
2026-05-18 22:02:41 +0100

    updated  Dockergroovy file
    Removed unused methods and streamlined the image build process.

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/env groovy
+
 package com.example
 
 class Docker implements Serializable {
@@ -9,20 +10,17 @@ class Docker implements Serializable {
         this.script = script
     }
     def buildDockerImage(String imageName) {
-        script. echo "building the docker image..."
+        script.echo "building the docker image..."
+        script.sh "docker build -t $imageName ."
+        
+    }
+    def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
-            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-            script.sh "docker push $imageName"
+            script.sh "echo 'script.PASS | docker login -u $script.USER --password-stdin"
         }
     }
-    // def dockerLogin() {
-    //     script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    //         script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-    //     }
-    // }
 
-    // def dockerPush(String imageName) {
-    //     script.sh "docker push $imageName"
-    // }
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
+    }
 }
